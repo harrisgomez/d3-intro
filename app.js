b60b5c5995e4f2e16fd896372e4b575e2ecec55c
harrisgomez <node.harris@gmail.com>
2020-11-13 18:45:42 -0500

    Added bars to SVG using d3-scale.

--- app.js
+++ app.js
@@ -12,14 +12,43 @@ const DUMMY_DATA = [
 //     .append('p')                    // renders <p> nodes for every data point
 //     .text(data => data.region);     // inserts the text/data
 
-const container = d3.select('div')
-    .classed('container', true)         // 2nd argument determines whether it should be added in or not
-    .style('border', '1px solid red');  // inline style
+// -----
 
-container.selectAll('.bar')
+// d3 with <div> container
+// const container = d3.select('div')
+//     .classed('container', true)         // 2nd argument determines whether it should be added in or not
+
+// container.selectAll('.bar')
+//     .data(DUMMY_DATA)
+//     .enter()
+//     .append('div')
+//     .classed('bar', true)               // adds the 'bar' class to the newly rendered divs
+//     .style('width', '50px')
+//     .style('height', data => (data.value *15) + 'px');
+
+// -----
+
+// Functions for calculating positions on the X and Y axis
+// (requires 'd3-scale' module installed)
+const xScale = d3.scaleBand()   // Data appears to best fit an Ordinal scale (same width bars)
+    .domain(DUMMY_DATA.map(dataPoint => dataPoint.region))  // lets scaling fn know how many data points we're working with in total
+    .rangeRound([0, 250])       // To and From values denoting how much space is available (width of container)
+    .padding(0.1)               // % padding between bars
+
+const yScale = d3.scaleLinear()
+    .domain([0, 10])            // Sets the upper/lower limits of Y-axis, letting d3 know the range of values it will be dealing with. In this case, 9 is the highest value in the data, so 10 will allow it some room
+    .range([200, 0])            // height of container
+
+// d3 with <svg> container
+const container = d3.select('svg')
+    .classed('container', true)         // 2nd argument determines whether it should be added in or not
+
+const bars = container.selectAll('.bar')
     .data(DUMMY_DATA)
     .enter()
-    .append('div')
-    .classed('bar', true)               // adds the 'bar' class to the newly rendered divs
-    .style('width', '50px')
-    .style('height', data => (data.value *15) + 'px');
+    .append('rect')                                     // div -> rect
+    .classed('bar', true)                               // adds the 'bar' class to the newly rendered divs
+    .attr('width', xScale.bandwidth())                  // style -> attr. Values automatically treated as 'px'. Can dynamically render values returned by d3-scale fns
+    .attr('height', data => 200 - yScale(data.value))   // need to factor in actual container height since d3 starts plotting from the top-left corner
+    .attr('x', data => xScale(data.region))
+    .attr('y', data => yScale(data.value))
