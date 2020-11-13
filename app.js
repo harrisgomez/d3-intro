71947e13bcd401a1d9b7ad095f316c56d15b6e89
harrisgomez <node.harris@gmail.com>
2020-11-13 18:01:36 -0500

    Rendered dynamic values.

--- app.js
+++ app.js
@@ -1,18 +1,25 @@
 const DUMMY_DATA = [
-    { id: 'd1', value: 10, region: 'USA'},
-    { id: 'd2', value: 20, region: 'China'},
-    { id: 'd3', value: 30, region: 'India'},
-    { id: 'd4', value: 40, region: 'Philippines'},
-    { id: 'd5', value: 50, region: 'Moscow'},
-]
+    { id: 'd1', value: 4, region: 'USA'},
+    { id: 'd2', value: 6, region: 'China'},
+    { id: 'd3', value: 9, region: 'India'},
+    { id: 'd4', value: 5, region: 'Philippines'}
+];
 
 // d3.select('div')                    // grabs div
 //     .selectAll('p')                 // selects all <p> nodes within <div>
 //     .data(DUMMY_DATA)               // data to be used
-//     .enter()                        // returns all the missing elements to which the data is bound
-//     .append('p')                    // appends those missing <p> nodes
+//     .enter()                        // returns all the data points
+//     .append('p')                    // renders <p> nodes for every data point
 //     .text(data => data.region);     // inserts the text/data
 
-d3.select('div')
+const container = d3.select('div')
     .classed('container', true)         // 2nd argument determines whether it should be added in or not
-    .style('border', '1px solid red')   // inline style
+    .style('border', '1px solid red');  // inline style
+
+container.selectAll('.bar')
+    .data(DUMMY_DATA)
+    .enter()
+    .append('div')
+    .classed('bar', true)               // adds the 'bar' class to the newly rendered divs
+    .style('width', '50px')
+    .style('height', data => (data.value *15) + 'px');
